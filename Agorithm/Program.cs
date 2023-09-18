406ba8ed97d1d57cf4a06f73727d709fc03f4890
Williams Abraham <wabraham@vatebra.com>
2023-09-18 12:10:30 +0100

    Add project files.

---
+++ Agorithm/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+static int LengthOfLongestSubstring(string s)
+{
+    HashSet<char> set = new HashSet<char>();
+    int left = 0, right = 0, maxLength = 0;
+
+    while (right < s.Length)
+    {
+        if (!set.Contains(s[right]))
+        {
+            set.Add(s[right]);
+            maxLength = Math.Max(maxLength, right - left + 1);
+            right++;
+        }
+        else
+        {
+            set.Remove(s[left]);
+            left++;
+        }
+    }
+
+    return maxLength;
+}
+Console.WriteLine("Hello, World!{0}",LengthOfLongestSubstring("abcabcbb"));
